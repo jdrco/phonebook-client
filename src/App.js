950316b965ff6cc7496581ce7a3a7ef98bcdc44e
Jared Drueco <drueco03@gmail.com>
2022-08-21 23:24:16 -0600

    2.6 - 2.10 done. Installed axios and json-server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Filter from './components/Filter';
+import Person from './components/Person';
+import PersonForm from './components/PersonForm';
+import Persons from './components/Persons';
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+  const [newName, setNewName] = useState('Add a new name');
+  const [newNumber, setNewNumber] = useState('Add number');
+  const [filter, setFilter] = useState('');
+
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const personsToShow =
+    persons && filter !== ''
+      ? persons.filter(
+          (person) =>
+            person.name.toLowerCase().includes(filter.toLowerCase()) === true
+        )
+      : persons;
+
+  const addNewPerson = (e) => {
+    e.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      window.confirm(`${newName} is already added to the phonebook`);
+    } else {
+      const person = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+
+      setPersons(persons.concat(person));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Phonebook</h2>
+
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <PersonForm
+        addNewPerson={addNewPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons personsToShow={personsToShow} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Persons.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Person from './Person';
+
+const Persons = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map((person) => (
+        <Person key={person.id} name={person.name} number={person.number} />
+      ))}
+    </div>
+  );
+};
+
+export default Persons;
